3a0c6a585db97eeeab5db67e4a4b26731768578b
zhangyongsheng <zhangyongsheng@youzan.com>
2020-05-21 22:06:51 +0800

    test: add test when no target

--- test/test.js
+++ test/test.js
@@ -6,6 +6,20 @@ const { execFileSync } = require('child_process');
 
 const updateSnapshots = process.argv[2] === 'update-snapshots';
 
+const getOutputWithoutTarget = dir => {
+  const entrydir = resolve(__dirname, 'fixtures', dir, 'out');
+  const origcwd = process.cwd();
+
+  process.chdir(entrydir);
+  const stdoutWithoutTarget = execFileSync(process.execPath, [
+    resolve(__dirname, '../gen-esm-wrapper.js'),
+    resolve(__dirname, `fixtures/${dir}/index.js`)
+  ]);
+  process.chdir(origcwd);
+
+  return stdoutWithoutTarget;
+}
+
 for (const dir of fs.readdirSync(resolve(__dirname, 'fixtures'))) {
   console.log(`Testing in directory ${dir}`);
   execFileSync(process.execPath, [
@@ -13,6 +27,9 @@ for (const dir of fs.readdirSync(resolve(__dirname, 'fixtures'))) {
     resolve(__dirname, `fixtures/${dir}/index.js`),
     resolve(__dirname, `fixtures/${dir}/out/wrapper.mjs`)
   ]);
+
+  // trim the last '\n'
+  const outputWithoutTarget = getOutputWithoutTarget(dir).slice(0, -1);
 
   const actual = fs.readFileSync(
     resolve(__dirname, `fixtures/${dir}/out/wrapper.mjs`), 'utf8');
@@ -23,5 +40,6 @@ for (const dir of fs.readdirSync(resolve(__dirname, 'fixtures'))) {
     const expected = fs.readFileSync(
       resolve(__dirname, `fixtures/${dir}/wrapper.mjs`), 'utf8');
     assert.strictEqual(actual, expected);
+    assert.strictEqual(Buffer.from(expected).equals(outputWithoutTarget), true);
   }
 }
